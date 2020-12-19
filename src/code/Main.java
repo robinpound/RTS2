8040efbe8bba5c11a3095a535026c277fd3c1095
robinpound <54936470+robinpound@users.noreply.github.com>
2020-12-19 20:45:32 +0000

    MY STUFF. KEEP YOU GRUBBY LITTLE STEVE WILLIAM BEET HANDS OFF MY CODE

--- src/code/Main.java
+++ src/code/Main.java
@@ -14,6 +14,8 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 import java.util.List;
 
 public class Main extends Application {
@@ -21,15 +23,15 @@ public class Main extends Application {
     Boolean CloseClicked = false;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        LoginMenu();
-        BuildingMenu();
-        SimulationMenu();
-    }
-    private void LoginMenu(){
         Stage secondaryStage = new Stage();
+        LoginMenu(secondaryStage);
+        BuildingMenu(secondaryStage);
+        SimulationMenu(secondaryStage);
+    }
+    private void LoginMenu(Stage secondaryStage){
 
         //set up Stage
-        secondaryStage.initModality(Modality.WINDOW_MODAL);
+        secondaryStage.initModality(Modality.APPLICATION_MODAL);
         secondaryStage.setTitle("Main Menu");
 
         //set up GridPane
@@ -90,16 +92,18 @@ public class Main extends Application {
         Scene menuScene = new Scene(menuGrid);
         secondaryStage.setScene(menuScene);
         secondaryStage.showAndWait();
-        secondaryStage.close();
 
     }
-    public void BuildingMenu(){
-        Stage thirdStage = new Stage();
-        thirdStage.initModality(Modality.WINDOW_MODAL);
+    private void BuildingMenu(Stage secondaryStage){
 
-        NormalUserInterface FirstUI = new NormalUserInterface(650, 950,thirdStage);
+        //set up Stage
+        //thirdStage.initModality(Modality.APPLICATION_MODAL);
+        secondaryStage.setTitle("Building Menu");
 
-        FirstUI.createGridPane(250);
+        NormalUserInterface FirstUI = new NormalUserInterface(650, 950, secondaryStage);
+
+        //Initial configurations
+        FirstUI.createGridPane(250, 0, 2);
         FirstUI.addStageDimensions();
         FirstUI.addCameraAndSubscene();
         FirstUI.setSimulation();
@@ -135,7 +139,7 @@ public class Main extends Application {
         FirstUI.NormalButtonHashMap.get("Launch").GetButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                thirdStage.close();
+                secondaryStage.close();
                 System.out.println(FirstUI.NormalButtonHashMap.get("Launch").Gettext() + " was clicked");
             }
         });
@@ -143,23 +147,20 @@ public class Main extends Application {
         FirstUI.NormalButtonHashMap.get("Exit").GetButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                thirdStage.close();
+                secondaryStage.close();
                 System.out.println(FirstUI.NormalButtonHashMap.get("Exit").Gettext() + " was clicked");
                 System.exit(0);
             }
         });
 
 
-
         FirstUI.GetStage().showAndWait();
 
     }
-    private void SimulationMenu() {
-        Stage fourthStage = new Stage();
-        fourthStage.initModality(Modality.WINDOW_MODAL);
-        NormalUserInterface SecondUI = new NormalUserInterface(700, 1000, fourthStage);
+    private void SimulationMenu(Stage secondaryStage) {
+        NormalUserInterface SecondUI = new NormalUserInterface(700, 1000, secondaryStage);
 
-        SecondUI.createGridPane(250);
+        SecondUI.createGridPane(250,0,2);
         SecondUI.addStageDimensions();
         SecondUI.addCameraAndSubscene();
         SecondUI.setSimulation();
@@ -216,14 +217,11 @@ public class Main extends Application {
         SecondUI.NormalButtonHashMap.get("Exit").GetButton().setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                fourthStage.close();
+                secondaryStage.close();
                 System.out.println(SecondUI.NormalButtonHashMap.get("Exit").Gettext() + " was clicked");
                 System.exit(0);
             }
         });
-
-        fourthStage.showAndWait();
-        fourthStage.close();
 
     }
 
